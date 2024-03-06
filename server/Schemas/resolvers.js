36a90dbacecb42796bee61144ddfbd9c1e182077
philsloan <pjs.tgj@gmail.com>
2024-03-06 12:18:34 -0500

    schemas

---
+++ server/Schemas/resolvers.js
@@ -0,0 +1,13 @@
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
